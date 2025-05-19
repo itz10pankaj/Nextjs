0b53cabc97c080ac7ac08ab1c4caa8495b9fe240
itz10pankaj <pgarg9355@gmail.com>
2025-05-19 16:33:13 +0530

    Structur imporoved

--- my-app/next-sitemap.config.js
+++ my-app/next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'http://localhost:3000', // Replace with your actual domain
-  generateRobotsTxt: true,           // (optional) Generate robots.txt file
+  generateRobotsTxt: false,   
+  exclude: ['/api/*']        // (optional) Generate robots.txt file
   // You can add more options if needed, like excluding certain pages
 };
