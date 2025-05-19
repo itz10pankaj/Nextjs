139846647bd323f3eb851c027ece21c463a83e0c
itz10pankaj <pgarg9355@gmail.com>
2025-05-19 16:09:02 +0530

    Testing Issues

--- my-app/src/app/api/header/route.ts
+++ my-app/src/app/api/header/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import links from "@/utlis/headerData";
+import links from "@/utlis/headerData.json";
 export async function GET() {
   return NextResponse.json(links);
 }
